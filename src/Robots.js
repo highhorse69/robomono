c5b2f7476f2ea1c395492a42da593fbffb91c6d2
Rohan Sahani <rohan.sahani.m97@gmail.com>
2021-02-19 04:50:22 +0530

    added

---
+++ src/Robots.js
@@ -0,0 +1,76 @@
+export const robots=[
+{
+	id: 1,
+	name: 'Rohan',
+	username: 'highhorse',
+	email: 'rohan.sahani@gmail.com'
+
+},
+
+{
+	id: 2,
+	name: 'Naina',
+	username: 'bhalla',
+	email: 'naina.chullbully@gmail.com'
+	
+
+},
+{
+	id: 3,
+	name: 'Shubham',
+	username: 'chutreti',
+	email: 'shubham.kukreti@gmail.com'
+
+},
+{
+	id: 4,
+	name: 'Kishan',
+	username: 'PANDEYJI',
+	email: 'kishan.pamdey@gmail.com'
+},
+{
+	id: 5,
+	name: 'Mehak',
+	username: 'kappor',
+	email: 'mehak.chinki@gmail.com'
+
+},
+{
+	id: 6,
+	name: 'Ankit',
+	username: 'hhorse',
+	email: 'dropout.prv.lmt.@gmail.com'
+
+}
+,{
+	id: 7,
+	name: 'Sagar',
+	username: 'highse',
+	email: 'Sagar.pahadi@gmail.com'
+
+},{
+	id: 8,
+	name: 'Nikhil',
+	username: 'ghhorse',
+	email: 'nikhil.khari@gmail.com'
+
+},{
+	id: 9,
+	name: 'Dympy',
+	username: 'highhor',
+	email: 'Dympy.hero@gmail.com'
+
+},{
+	id: 10,
+	name: 'Vishal',
+	username: 'highhors',
+	email: 'jewar.boy@gmail.com'
+
+},{
+	id: 11,
+	name: 'Aman',
+	username: 'highrse',
+	email: 'aman.swimmer@gmail.com'
+
+}
+]
